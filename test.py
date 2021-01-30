d7622dc5f34e4abf2076d6c707d94db7de9e5b2f
Tianwei Cao <78243340+TianweiCao@users.noreply.github.com>
2021-01-30 22:57:49 +0800

    Update test.py

--- test.py
+++ test.py
@@ -16,7 +16,7 @@ def subtract(a, b):
 # ============================================================
 
 def test_add():
-    assert add(2, 3) == 5
+    assert add(2, 3) == 50
     assert add('boston', 'university') == 'bostonuniversity'
 
 
