257f5c553cf6406954fe1fdf53567649763bee0a
Tianwei Cao <78243340+TianweiCao@users.noreply.github.com>
2021-01-30 21:23:48 +0800

    Update test.py

--- test.py
+++ test.py
@@ -4,3 +4,27 @@ def foo(s):
   return 10/n
 def test_foo():
   assert foo(10)==1
+
+  import numpy as np
+# ============================================================
+# Defining your own functions here
+# ============================================================
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+# ============================================================
+# Defining your own testing here
+# ============================================================
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add('boston', 'university') == 'bostonuniversity'
+
+
+def test_subtract():
+    assert subtract(2, 3) == -1
+© 2021 GitHub, Inc.
